253f2fa3722ebf0d1c46079dbeb59bfc8855e6e8
cassandraortiz <cassy.ortiz5@gmail.com>
2020-09-22 10:25:48 -0600

    Lesson Repo did not clone.  Completed Lesson work here.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,11 @@
 # Write your #display_rainbow method here
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+def display_rainbow(arr)
+    displayText = " "
+    for i in 0..arr.length-1
+        displayText = displayText + "#{arr[i][0,1].capitalize}: #{arr[i]}, "
+    end
+    displayText = displayText.delete_suffix(', ')
+    puts displayText
+end
